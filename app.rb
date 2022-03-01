c1a7886316741603fc751a2801d5e343f3778555
erik-cairns-nti-johanneberg <erik.cairns@elev.ga.ntig.se>
2022-03-01 15:07:24 +0100

    Allt med inlogg är färdigt
    Fungerande inlogg registrering och allt

--- app.rb
+++ app.rb
@@ -9,21 +9,70 @@ get('/') do #visa startsida
   slim(:start)
 end
 
-get('/showcards') do
-    db = SQLite3::Database.new('/db/wsp22_db.db')
-    db.results_as_hash = true
-    all_digimon = db.execute("SELECT * FROM digimon")
-    slim(:"digimon/index",locals:{digimon:all_digimon})
+get('/register') do #visa registreringssidan
+    slim(:register)
 end
 
-post('/users/new') do #registrera användrae
+get ('/login') do #visa loginsidan
+    slim(:login)
+end
+
+post('/users/new') do#registrerara användare.
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if password == password_confirm
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/wsp22_db.db')
+      begin 
+        db.execute("INSERT INTO user (username,pswdig,money) VALUES (?,?,?)",username,password_digest, 100)
+        redirect('/login')
+      rescue => exeption
+        #säg att användarnamnet redan finns
+        redirect('/')
+      end
+
+
+  
+    else 
+  
+      "lösenorden matchade inte"
+      redirect('/register')
+    end
+end
+
+post('/users/login') do
     username=params[:username]
     password=params[:password]
-    password_conf=params[:password_confirm]
-
-    if (password_confirm=password)
-        #lägg till användeare
-    else
-        #hantera fel lösen
+  
+    db = SQLite3::Database.new('db\wsp22_db.db')
+    db.results_as_hash = true
+  
+    result = db.execute('SELECT * FROM user WHERE username =?', username).first #ta alla med önskat username
+    
+    if result == nil #undantagshantera ett användarnamn som inte finns
+      redirect('/errorusername')
     end
+
+    pswdig = result["pswdig"]
+    id=result["id"]
+  
+    if BCrypt::Password.new(pswdig) == password
+      session[:id] = id
+      session[:inloggad]=true
+     redirect('/')
+    else
+      redirect('/')
+    end
+end
+
+get ('/loggaut') do # logga ut anvädare 
+  session[:inloggad] = false
+  redirect('/')
+end
+
+get ('/errorusername') do #visa fel användare
+  #visa de va fel
+  redirect('/login')
 end
